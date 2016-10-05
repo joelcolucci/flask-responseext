9ee0bb2a4a27c81a9e8b26fa8cf70c3c944d9702
Joel Colucci <joelcolucci@gmail.com>
2016-10-05 07:43:33 -0400

    feat: implement set_status method

--- flask_responseext/response.py
+++ flask_responseext/response.py
@@ -1,5 +1,7 @@
-"""Module contains Response class. The Response class follows the factory
-pattern for producing Flask's Response object with applicable HTTP status codes.
+"""
+    flask_responseext.response
+
+    Contains Response subclass of Flask Response object
 """
 
 
@@ -9,7 +11,13 @@ from flask import Response as FlaskResponse
 
 
 class Response(FlaskResponse):
-    """Flask Response class with extended functionality"""
+    """Flask Response subclass providing ability to chain
+    setter methods"""
 
-    def __init__(self):
-        pass
+    def __init__(self, rv=None, **kwargs):
+        FlaskResponse.__init__(self, rv, **kwargs)
+
+    def set_status(self, status_code):
+        self.status_code = status_code
+
+        return self

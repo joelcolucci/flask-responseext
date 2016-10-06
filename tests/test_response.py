5fd89c6d35e18bf3a6ae19b39879f8aa74a482c8
Joel Colucci <joelcolucci@gmail.com>
2016-10-06 06:25:37 -0400

    tests: add tests for set_headers method

--- tests/test_response.py
+++ tests/test_response.py
@@ -31,12 +31,33 @@ class ResponseTestCase(TestCase):
         self.assertEqual(response.status_code, 404)
 
     def test_set_status_returns_self(self):
+        """Test method returns self"""
         response = Response()
 
         return_val = response.set_status(404)
 
         self.assertIsInstance(return_val, Response)
 
+    def test_set_headers_success(self):
+        """Test method assigns headers to Response object"""
+        response = Response()
+
+        response.set_headers({'Access-Control-Allow-Origin': '*'})
+
+        accessControlMatcher = response.headers['Access-Control-Allow-Origin']
+        
+        self.assertEqual(accessControlMatcher, '*')
+
+    def test_set_headers_returns_self(self):
+        """Test method returns self"""
+        response = Response()
+
+        return_val = response.set_headers({'Access-Control-Allow-Origin': '*'})
+
+        self.assertIsInstance(return_val, Response)
+
+    def test_to_json_success(self):
+        pass
 
 if __name__ == '__main__':
     main()
